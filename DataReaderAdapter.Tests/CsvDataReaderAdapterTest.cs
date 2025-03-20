417b09c4d698bab4183e9ec90d9cc0a215d1e441
usausa <machi.pon@gmail.com>
2025-03-20 16:44:55 +0900

    Update test

--- DataReaderAdapter.Tests/CsvDataReaderAdapterTest.cs
+++ DataReaderAdapter.Tests/CsvDataReaderAdapterTest.cs
@@ -1,10 +1,64 @@
 namespace DataReaderAdapter.Tests;
 
+using System.Globalization;
+
+using CsvHelper;
+
 public class CsvDataReaderAdapterTest
 {
     [Fact]
-    public void Test1()
+    public void TestBasic()
     {
-        Assert.True(true);
+        var content =
+            "Col1,Col2,Col3,Col4,Col5\n" +
+            "1,,10,,A\n" +
+            "2,,,,B\n" +
+            "3,,30,,";
+        using var csv = new CsvReader(new StringReader(content), CultureInfo.InvariantCulture);
+
+        var option = new CsvDataReaderOption();
+        option.AddColumn("Col1");
+        option.AddColumn("Col3", emptyAsNull: true);
+        option.AddColumn("Col5", emptyAsNull: true);
+
+        using var reader = new CsvDataReaderAdapter(option, csv);
+
+        // Assert
+        Assert.Equal(3, reader.FieldCount);
+
+        // 1st
+        Assert.True(reader.Read());
+
+        Assert.False(reader.IsDBNull(1));
+        Assert.False(reader.IsDBNull(2));
+
+        var values = new object[reader.FieldCount];
+        reader.GetValues(values);
+        Assert.Equal("1", values[0]);
+        Assert.Equal("10", values[1]);
+        Assert.Equal("A", values[2]);
+
+        // 2nd
+        Assert.True(reader.Read());
+
+        Assert.True(reader.IsDBNull(1));
+        Assert.False(reader.IsDBNull(2));
+
+        Assert.Equal("2", reader.GetValue(0));
+        Assert.Equal(DBNull.Value, reader.GetValue(1));
+        Assert.Equal("B", reader.GetValue(2));
+
+        // 3rd
+        Assert.True(reader.Read());
+
+        Assert.False(reader.IsDBNull(1));
+        Assert.True(reader.IsDBNull(2));
+
+        Assert.Equal("3", reader.GetValue(0));
+        Assert.Equal("30", reader.GetValue(1));
+        Assert.Equal(DBNull.Value, reader.GetValue(2));
+
+        // Completed
+        Assert.False(reader.Read());
     }
 }
--- DataReaderAdapter.Tests/ObjectDataReaderAdapterTest.cs
+++ DataReaderAdapter.Tests/ObjectDataReaderAdapterTest.cs
@@ -3,8 +3,69 @@ namespace DataReaderAdapter.Tests;
 public class ObjectDataReaderAdapterTest
 {
     [Fact]
-    public void Test1()
+    public void TestBasic()
     {
-        Assert.True(true);
+        var list = new List<Data>
+        {
+            new() { IntValue = 1, NullableIntValue = 10, StringValue = "A" },
+            new() { IntValue = 2, StringValue = "B" },
+            new() { IntValue = 3, NullableIntValue = 30 }
+        };
+
+        using var reader = new ObjectDataReaderAdapter<Data>(list);
+
+        // Assert
+        Assert.Equal(3, reader.FieldCount);
+
+        var intIndex = reader.GetOrdinal(nameof(Data.IntValue));
+        var nullableIntIndex = reader.GetOrdinal(nameof(Data.NullableIntValue));
+        var stringIndex = reader.GetOrdinal(nameof(Data.StringValue));
+        Assert.Equal(0, intIndex);
+        Assert.Equal(1, nullableIntIndex);
+        Assert.Equal(2, stringIndex);
+
+        // 1st
+        Assert.True(reader.Read());
+
+        Assert.False(reader.IsDBNull(nullableIntIndex));
+        Assert.False(reader.IsDBNull(stringIndex));
+
+        var values = new object[reader.FieldCount];
+        reader.GetValues(values);
+        Assert.Equal(1, values[intIndex]);
+        Assert.Equal(10, values[nullableIntIndex]);
+        Assert.Equal("A", values[stringIndex]);
+
+        // 2nd
+        Assert.True(reader.Read());
+
+        Assert.True(reader.IsDBNull(nullableIntIndex));
+        Assert.False(reader.IsDBNull(stringIndex));
+
+        Assert.Equal(2, reader.GetValue(intIndex));
+        Assert.Equal(DBNull.Value, reader.GetValue(nullableIntIndex));
+        Assert.Equal("B", reader.GetValue(stringIndex));
+
+        // 3rd
+        Assert.True(reader.Read());
+
+        Assert.False(reader.IsDBNull(nullableIntIndex));
+        Assert.True(reader.IsDBNull(stringIndex));
+
+        Assert.Equal(3, reader.GetValue(intIndex));
+        Assert.Equal(30, reader.GetValue(nullableIntIndex));
+        Assert.Equal(DBNull.Value, reader.GetValue(stringIndex));
+
+        // Completed
+        Assert.False(reader.Read());
+    }
+
+    private sealed class Data
+    {
+        public int IntValue { get; set; }
+
+        public int? NullableIntValue { get; set; }
+
+        public string? StringValue { get; set; }
     }
 }
