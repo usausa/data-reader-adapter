b1efcf3961888142cb2b0af8e645b99c3fddc092
usausa <machi.pon@gmail.com>
2025-03-20 16:54:41 +0900

    Update test

--- DataReaderAdapter.Tests/CsvDataReaderAdapterTest.cs
+++ DataReaderAdapter.Tests/CsvDataReaderAdapterTest.cs
@@ -26,37 +26,44 @@ public class CsvDataReaderAdapterTest
         // Assert
         Assert.Equal(3, reader.FieldCount);
 
+        var col1Index = reader.GetOrdinal("Col1");
+        var col3Index = reader.GetOrdinal("Col3");
+        var col5Index = reader.GetOrdinal("Col5");
+        Assert.Equal(0, col1Index);
+        Assert.Equal(1, col3Index);
+        Assert.Equal(2, col5Index);
+
         // 1st
         Assert.True(reader.Read());
 
-        Assert.False(reader.IsDBNull(1));
-        Assert.False(reader.IsDBNull(2));
+        Assert.False(reader.IsDBNull(col3Index));
+        Assert.False(reader.IsDBNull(col5Index));
 
         var values = new object[reader.FieldCount];
         reader.GetValues(values);
-        Assert.Equal("1", values[0]);
-        Assert.Equal("10", values[1]);
-        Assert.Equal("A", values[2]);
+        Assert.Equal("1", values[col1Index]);
+        Assert.Equal("10", values[col3Index]);
+        Assert.Equal("A", values[col5Index]);
 
         // 2nd
         Assert.True(reader.Read());
 
-        Assert.True(reader.IsDBNull(1));
-        Assert.False(reader.IsDBNull(2));
+        Assert.True(reader.IsDBNull(col3Index));
+        Assert.False(reader.IsDBNull(col5Index));
 
-        Assert.Equal("2", reader.GetValue(0));
-        Assert.Equal(DBNull.Value, reader.GetValue(1));
-        Assert.Equal("B", reader.GetValue(2));
+        Assert.Equal("2", reader.GetValue(col1Index));
+        Assert.Equal(DBNull.Value, reader.GetValue(col3Index));
+        Assert.Equal("B", reader.GetValue(col5Index));
 
         // 3rd
         Assert.True(reader.Read());
 
-        Assert.False(reader.IsDBNull(1));
-        Assert.True(reader.IsDBNull(2));
+        Assert.False(reader.IsDBNull(col3Index));
+        Assert.True(reader.IsDBNull(col5Index));
 
-        Assert.Equal("3", reader.GetValue(0));
-        Assert.Equal("30", reader.GetValue(1));
-        Assert.Equal(DBNull.Value, reader.GetValue(2));
+        Assert.Equal("3", reader.GetValue(col1Index));
+        Assert.Equal("30", reader.GetValue(col3Index));
+        Assert.Equal(DBNull.Value, reader.GetValue(col5Index));
 
         // Completed
         Assert.False(reader.Read());
