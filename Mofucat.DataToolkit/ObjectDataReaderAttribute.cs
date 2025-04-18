e1946161053f7fd29a602d1818d41ca0020a31c3
usausa <machi.pon@gmail.com>
2025-04-18 15:48:35 +0900

    Update reader

--- Mofucat.DataToolkit/ObjectDataReaderAttribute.cs
+++ Mofucat.DataToolkit/ObjectDataReaderAttribute.cs
@@ -1,3 +1,17 @@
 namespace Mofucat.DataToolkit;
 
-// TODO Index, Ignore
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class DataColumnAttribute : Attribute
+{
+    public int Order { get; }
+
+    public DataColumnAttribute(int order)
+    {
+        Order = order;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class DataIgnoreAttribute : Attribute
+{
+}
--- Mofucat.DataToolkit/ObjectDataReaderOption.cs
+++ Mofucat.DataToolkit/ObjectDataReaderOption.cs
@@ -4,14 +4,24 @@ using System.Collections.Concurrent;
 using System.Linq.Expressions;
 using System.Reflection;
 
-// TODO Default selector with attribute
-
 public sealed class ObjectDataReaderOption<T>
 {
+    private static readonly PropertyInfo[] DefaultSelector =
+        typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Where(static x => x.CanRead && (x.GetCustomAttribute<DataIgnoreAttribute>() is not null))
+            .Select(static (x, i) =>
+            {
+                var order = x.GetCustomAttribute<DataColumnAttribute>()?.Order ?? Int32.MaxValue;
+                return new { Order = order, Index = i, Property = x };
+            })
+            .OrderBy(static x => x.Order)
+            .ThenBy(static x => x.Index)
+            .Select(static x => x.Property)
+            .ToArray();
+
     private static readonly ConcurrentDictionary<PropertyInfo, Func<T, object?>> Accessors = new();
 
-    public Func<IEnumerable<PropertyInfo>> PropertySelector { get; set; } =
-        static () => typeof(T).GetProperties();
+    public Func<IEnumerable<PropertyInfo>> PropertySelector { get; set; } = () => DefaultSelector;
 
     public Func<PropertyInfo, Func<T, object?>> AccessorFactory { get; set; } = DefaultFactory;
 
